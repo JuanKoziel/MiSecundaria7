8077d9e4f1aba4ee83b32c5cda4622c6755c1349
Koziel <kozieljuanpablo8@gmail.com>
2026-09-07 02:42:05 -0300

    .

--- frontend/src/components/Preceptores/sidebarMenu.js
+++ frontend/src/components/Preceptores/sidebarMenu.js
@@ -3,7 +3,7 @@ export const menuItems = [
   { id: 'alumnos', label: 'Estudiantes', icon: 'fa-users' },
   { id: 'tutores', label: 'Tutores', icon: 'fa-user-shield' },
   { id: 'docentes', label: 'Docentes', icon: 'fa-chalkboard-teacher' },
-  { id: 'horarios', label: 'Horarios', icon: 'fa-calendar-alt' },
+  { id: 'horarios', label: 'Horarios', icon: 'fa-clock' },
   { id: 'adelantos-horas', label: 'Adelantos de Horas', icon: 'fa-forward' },
   { id: 'asistencias', label: 'Asistencias', icon: 'fa-calendar-check' },
   { id: 'notas', label: 'Calificaciones', icon: 'fa-clipboard-list' },
